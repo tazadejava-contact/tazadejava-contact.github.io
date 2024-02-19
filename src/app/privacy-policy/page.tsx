8f96c88de7c95b14714fb0a7d220f040a2298658
gooba <tonitruirrein@gmail.com>
2024-02-19 18:04:33 -0500

    Add privacy policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -7,7 +7,7 @@ export default function Page(): JSX.Element {
             Privacy Policy
         </div>
         <div className="text-center">
-            Budget Buddy runs completely offline. Expenses are tracked manually and stored securely on the phone's local filesystem. No budget category, expense, or transaction data is shared to any outside parties aside from the user of the app. The app provides an option to export budget expenses. However, expenses are exported at the discretion of the user and Budget Buddy is not responsible for exposed information, i.e. if the data is placed in an insecure location. Happy budgeting!
+            {"Budget Buddy runs completely offline. Expenses are tracked manually and stored securely on the phone's local filesystem. No budget category, expense, or transaction data is shared to any outside parties aside from the user of the app. The app provides an option to export budget expenses. However, expenses are exported at the discretion of the user and Budget Buddy is not responsible for exposed information, i.e. if the data is placed in an insecure location. Happy budgeting!"}
         </div>
     </div>
 }
